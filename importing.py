fa56f9150725664442d63cbbb6736b2f09d1e844
Matt Fender <mjfender@gmail.com>
2017-04-02 19:18:34 -0400

    import lesson

---
+++ importing.py
@@ -0,0 +1,11 @@
+from math import sqrt
+# this imports only the function sqrt
+print sqrt(25)
+
+""" another alternative would be 'import math'
+and then 'print math.sqrt(25)' """
+
+""" another alternative would be 'universal import'
+'from math import *'
+Note: this is not recommended because you don't know
+which functions you're getting """
